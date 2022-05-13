f1ffc8967102cdb2e11825048a1c51b54ad949e8
Admin <mdoeden@unomaha.edu>
2022-05-12 19:07:57 -0500

    Added GUI tutorials and other usability changes.

--- main.py
+++ main.py
@@ -10,22 +10,22 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.Surface((0, 0))
         self.image.fill((0, 0, 0))
         self.rect = self.image.get_rect(center=(400, 750))
-        self.current_health = 50
+        self.current_health = 90
         self.maximum_health = 100
         self.health_bar_length = 400
         self.health_ratio = self.maximum_health / self.health_bar_length
 
-        self.current_work = 50
+        self.current_work = 90
         self.maximum_work = 100
         self.work_bar_length = 400
         self.work_ratio = self.maximum_work / self.work_bar_length
 
-        self.current_school = 50
+        self.current_school = 90
         self.maximum_school = 100
         self.school_bar_length = 400
         self.school_ratio = self.maximum_school / self.school_bar_length
 
-        self.current_family = 50
+        self.current_family = 90
         self.maximum_family = 100
         self.family_bar_length = 400
         self.family_ratio = self.maximum_family / self.family_bar_length
@@ -86,20 +86,20 @@ class Player(pygame.sprite.Sprite):
             self.current_family = self.maximum_family
 
     def basic_health(self):
-        pygame.draw.rect(screen, (255, 0, 0), (10, 100, self.current_health / self.health_ratio, 25))
-        pygame.draw.rect(screen, (255, 0, 0), (10, 100, self.health_bar_length, 25), 4)
+        pygame.draw.rect(screen, (255, 0, 0), (200, 100, self.current_health / self.health_ratio, 25))
+        pygame.draw.rect(screen, (255, 0, 0), (200, 100, self.health_bar_length, 25), 4)
 
     def work(self):
-        pygame.draw.rect(screen, (0, 255, 0), (10, 200, self.current_work / self.work_ratio, 25))
-        pygame.draw.rect(screen, (0, 255, 0), (10, 200, self.work_bar_length, 25), 4)
+        pygame.draw.rect(screen, (0, 255, 0), (200, 200, self.current_work / self.work_ratio, 25))
+        pygame.draw.rect(screen, (0, 255, 0), (200, 200, self.work_bar_length, 25), 4)
 
     def school(self):
-        pygame.draw.rect(screen, (0, 0, 255), (10, 300, self.current_school / self.school_ratio, 25))
-        pygame.draw.rect(screen, (0, 0, 255), (10, 300, self.school_bar_length, 25), 4)
+        pygame.draw.rect(screen, (0, 0, 255), (200, 300, self.current_school / self.school_ratio, 25))
+        pygame.draw.rect(screen, (0, 0, 255), (200, 300, self.school_bar_length, 25), 4)
 
     def family(self):
-        pygame.draw.rect(screen, (255, 0, 255), (10, 400, self.current_family / self.family_ratio, 25))
-        pygame.draw.rect(screen, (255, 0, 255), (10, 400, self.family_bar_length, 25), 4)
+        pygame.draw.rect(screen, (255, 0, 255), (200, 400, self.current_family / self.family_ratio, 25))
+        pygame.draw.rect(screen, (255, 0, 255), (200, 400, self.family_bar_length, 25), 4)
 
     def random_event(self):
         events = [0, 1, 2, 3]
@@ -120,24 +120,6 @@ class Player(pygame.sprite.Sprite):
             print('A big work project!')
             self.event_text = 3
             self.bleed_rate_multi = 3
-
-    def show_text(self, event):
-        if event == 0:
-            event_text = font.render('!!COVID!!', True, (random.randint(100, 255), 0, random.randint(100, 255)))
-            screen.blit(event_text, (10, 10))
-            pygame.display.update()
-        if event == 1:
-            event_text = font.render('!!FINALS!!', True, (random.randint(100, 255), 0, random.randint(100, 255)))
-            screen.blit(event_text, (10, 10))
-            pygame.display.update()
-        if event == 2:
-            event_text = font.render('!!A DEATH IN THE FAMILY!!', True, (random.randint(100, 255), 0, random.randint(100, 255)))
-            screen.blit(event_text, (10, 10))
-            pygame.display.update()
-        if event == 3:
-            event_text = font.render('!!A BIG WORK PROJECT!!', True, (random.randint(100, 255), 0, random.randint(100, 255)))
-            screen.blit(event_text, (10, 10))
-            pygame.display.update()
 
     def fail_state(self):
         if self.current_health <= 0:
@@ -172,6 +154,7 @@ screen = pygame.display.set_mode((800, 800))
 clock = pygame.time.Clock()
 user = pygame.sprite.GroupSingle(Player())
 font = pygame.font.Font('freesansbold.ttf', 32)
+tut_font = pygame.font.Font('freesansbold.ttf', 24)
 
 
 # click = False
@@ -181,11 +164,17 @@ def main_menu():
     while True:
 
         screen.fill((0, 0, 0))
-        title = font.render('LIFE SIMULATOR 2022', True, (random.randint(100, 255), 0, random.randint(100, 255)))
+        title = font.render('LIFE SIMULATOR 2022', True, (255, 255, 255))
         play_text = font.render('PLAY', True, (random.randint(100, 255), 0, random.randint(100, 255)))
         quit_text = font.render('QUIT', True, (random.randint(100, 255), 0, random.randint(100, 255)))
+        tut_text = tut_font.render('Use the ARROW KEYS to survive!', True,
+                                   (random.randint(100, 255), 0, random.randint(100, 255)))
+        high_text = tut_font.render('Go for a high score!', True,
+                                    (random.randint(100, 255), 0, random.randint(100, 255)))
         pygame.draw.rect(screen, (255, 255, 255), (200, 5, 400, 40), 4)
         screen.blit(title, (225, 10))
+        screen.blit(tut_text, (100, 100))
+        screen.blit(high_text, (100, 150))
 
         mx, my = pygame.mouse.get_pos()
 
@@ -259,15 +248,15 @@ def game_loop():
         title = font.render('LIFE SIMULATOR 2022', True, (255, 255, 255))
         score = font.render(f'Score = {user.sprite.score}', True, (255, 255, 255))
         high_score = font.render(f'High score = {read_score()}', True, (255, 255, 255))
-        event_choice = {0:'COVID', 1:'FINALS', 2:'A DEATH IN THE FAMILY', 3:'A BIG WORK PROJECT'}
+        event_choice = {0: 'COVID', 1: 'FINALS', 2: 'A DEATH IN THE FAMILY', 3: 'A BIG WORK PROJECT'}
         event_key = event_choice[user.sprite.event_text]
-        event = font.render(f'Event: {event_key}', True, (255,255,255))
+        event = font.render(f'Event: {event_key}', True, (255, 255, 255))
         quit_text = font.render('QUIT', True, (random.randint(100, 255), 0, random.randint(100, 255)))
         quit_button = pygame.Rect(300, 700, 200, 50)
         screen.blit(high_score, (10, 550))
         screen.blit(score, (10, 500))
         screen.blit(title, (225, 10))
-        screen.blit(event, (200,450))
+        screen.blit(event, (175, 450))
         pygame.draw.rect(screen, (255, 255, 255), (200, 5, 400, 40), 4)
         pygame.draw.rect(screen, (255, 255, 255), quit_button)
         screen.blit(quit_text, (360, 710))
@@ -279,5 +268,6 @@ def game_loop():
             if click:
                 pygame.quit()
                 sys.exit()
+
 
 main_menu()
