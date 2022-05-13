20664974c0dec23078497e6e7c2e49fdc7e4d59b
Admin <mdoeden@unomaha.edu>
2022-05-12 19:24:00 -0500

    Added documentation and type hinting.

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ import random
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self):
+    """
+    Main player class made to store all variables.
+    """
+
+    def __init__(self) -> None:
         super().__init__()
         self.image = pygame.Surface((0, 0))
         self.image.fill((0, 0, 0))
@@ -14,33 +18,29 @@ class Player(pygame.sprite.Sprite):
         self.maximum_health = 100
         self.health_bar_length = 400
         self.health_ratio = self.maximum_health / self.health_bar_length
-
         self.current_work = 90
         self.maximum_work = 100
         self.work_bar_length = 400
         self.work_ratio = self.maximum_work / self.work_bar_length
-
         self.current_school = 90
         self.maximum_school = 100
         self.school_bar_length = 400
         self.school_ratio = self.maximum_school / self.school_bar_length
-
         self.current_family = 90
         self.maximum_family = 100
         self.family_bar_length = 400
         self.family_ratio = self.maximum_family / self.family_bar_length
-
         self.score = 0
         self.game_over = False
-
         self.event_text = 0
-
         self.default_bleed_rate = random.uniform(0.05, 0.15)
         self.bleed_rate_multi = 1
-
         self.write_score(self.score)
 
     def update(self) -> None:
+        """
+        Function to collect variables that should be updated with tick.
+        """
         self.basic_health()
         self.work()
         self.school()
@@ -55,73 +55,97 @@ class Player(pygame.sprite.Sprite):
 
         self.score += 1
 
-    def get_damage(self, amount):
-        if self.current_health > 0:
-            self.current_health -= amount
-        if self.current_health <= 0:
-            self.current_health = 0
-
-    def add_health(self, amount):
+    def add_health(self, amount: int) -> None:
+        """
+        Function to increment health value based on player input events.
+        :param amount: integer to increment
+        """
         if self.current_health < self.maximum_health:
             self.current_health += amount
         if self.current_health >= self.maximum_health:
             self.current_health = self.maximum_health
 
-    def add_work(self, amount):
+    def add_work(self, amount) -> None:
+        """
+        Function to increment work value based on player input events.
+        :param amount: integer to increment
+        """
         if self.current_work < self.maximum_work:
             self.current_work += amount
         if self.current_work >= self.maximum_work:
             self.current_work = self.maximum_work
 
-    def add_school(self, amount):
+    def add_school(self, amount) -> None:
+        """
+        Function to increment school value based on player input events.
+        :param amount: integer to increment
+        """
         if self.current_school < self.maximum_school:
             self.current_school += amount
         if self.current_school >= self.maximum_school:
             self.current_school = self.maximum_school
 
-    def add_family(self, amount):
+    def add_family(self, amount) -> None:
+        """
+        Function to increment family value based on player input events.
+        :param amount: integer to increment
+        """
         if self.current_family < self.maximum_family:
             self.current_family += amount
         if self.current_family >= self.maximum_family:
             self.current_family = self.maximum_family
 
-    def basic_health(self):
+    def basic_health(self) -> None:
+        """
+        Function to draw and update health bar on screen.
+        """
         pygame.draw.rect(screen, (255, 0, 0), (200, 100, self.current_health / self.health_ratio, 25))
         pygame.draw.rect(screen, (255, 0, 0), (200, 100, self.health_bar_length, 25), 4)
 
-    def work(self):
+    def work(self) -> None:
+        """
+        Function to draw and update work bar on screen.
+        """
         pygame.draw.rect(screen, (0, 255, 0), (200, 200, self.current_work / self.work_ratio, 25))
         pygame.draw.rect(screen, (0, 255, 0), (200, 200, self.work_bar_length, 25), 4)
 
-    def school(self):
+    def school(self) -> None:
+        """
+        Function to draw and update school bar on screen.
+        """
         pygame.draw.rect(screen, (0, 0, 255), (200, 300, self.current_school / self.school_ratio, 25))
         pygame.draw.rect(screen, (0, 0, 255), (200, 300, self.school_bar_length, 25), 4)
 
-    def family(self):
+    def family(self) -> None:
+        """
+        Function to draw and update school bar on screen.
+        """
         pygame.draw.rect(screen, (255, 0, 255), (200, 400, self.current_family / self.family_ratio, 25))
         pygame.draw.rect(screen, (255, 0, 255), (200, 400, self.family_bar_length, 25), 4)
 
-    def random_event(self):
+    def random_event(self) -> None:
+        """
+        Function that utilizes random module to generate a random event which modifies the bleed_rate_multi.
+        """
         events = [0, 1, 2, 3]
         current_event = random.choice(events)
         if current_event == 0:
-            print('Covid!')
             self.event_text = 0
             self.bleed_rate_multi = 5
         if current_event == 1:
-            print('Finals!')
             self.event_text = 1
             self.bleed_rate_multi = 2
         if current_event == 2:
-            print('A death in the family!')
             self.event_text = 2
             self.bleed_rate_multi = 4
         if current_event == 3:
-            print('A big work project!')
             self.event_text = 3
             self.bleed_rate_multi = 3
 
-    def fail_state(self):
+    def fail_state(self) -> None:
+        """
+        Function to define the main fail loop and reset all variables to init state.
+        """
         if self.current_health <= 0:
             self.write_score(self.score)
             self.restart()
@@ -135,10 +159,17 @@ class Player(pygame.sprite.Sprite):
             self.write_score(self.score)
             self.restart()
 
-    def restart(self):
+    def restart(self) -> None:
+        """
+        Function to wrap init call for clarity.
+        """
         self.__init__()
 
-    def write_score(self, score):
+    def write_score(self, score: int) -> None:
+        """
+        Function to read/write score to txt file.
+        :param score: integer that tracks score.
+        """
         with open('highscore.txt', 'r') as file:
             last = int(file.read())
         if score < last:
@@ -148,6 +179,7 @@ class Player(pygame.sprite.Sprite):
                 file.write(str(score))
 
 
+# Pygame module initializations.
 pygame.init()
 pygame.freetype.init()
 screen = pygame.display.set_mode((800, 800))
@@ -157,10 +189,10 @@ font = pygame.font.Font('freesansbold.ttf', 32)
 tut_font = pygame.font.Font('freesansbold.ttf', 24)
 
 
-# click = False
-
-
 def main_menu():
+    """
+    Menu loop.
+    """
     while True:
 
         screen.fill((0, 0, 0))
@@ -214,6 +246,10 @@ def main_menu():
 
 
 def game_loop():
+    """
+    Game loop; this contains the majority of the screen code.
+    :return:
+    """
     while True:
         click = False
         for event in pygame.event.get():
@@ -270,4 +306,5 @@ def game_loop():
                 sys.exit()
 
 
+# Start game.
 main_menu()
